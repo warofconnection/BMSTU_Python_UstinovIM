adac80e5a3adefa9074f5d9da1a81445ca45f8f9
ilyau <ilya.ustinov.06@bk.ru>
2025-09-26 15:28:03 +0300

    1

--- laboratory_work_01/dop.py
+++ laboratory_work_01/dop.py
@@ -1,6 +1,6 @@
-from math import (pi, sin, cos, tan, log)
+from math import *
 x, y = input('Введите данные x, y: ').split()
-x, y= int(x), int(y)
-z1 = cos**4(x) + sin**2(y) + 1/4*sin**2(2*x) - 1
+x, y= float(x), float(y)
+z1 = cos(x)**4 + sin(y)**2 + 1/4*sin(2*x) - 1
 z2 = sin(y+x)*sin(y-x)
 print(z1, z2)
